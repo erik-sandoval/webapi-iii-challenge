3fbe2c5d9aebafd1851ded775eee381a6397f705
erik-sandoval <erik.sandoval@icloud.com>
2019-05-30 13:20:47 -0700

    update for heroku

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // code away!
 const server = require("./server");
 
-server.listen(4000, () => {
-  console.log("listening on port 4000");
+const port = process.env.PORT
+
+server.listen(port, () => {
+  console.log(`listening on port ${port}`);
 });
